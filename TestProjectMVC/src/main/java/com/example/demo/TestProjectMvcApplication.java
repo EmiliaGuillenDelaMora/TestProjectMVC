9a4587102c73fbed9696bac01d970b43867e6686
eDelaMora <eDelaMora@CLQL001412>
2019-11-19 23:17:38 -0600

    create master for TestProjectMVC

---
+++ TestProjectMVC/src/main/java/com/example/demo/TestProjectMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectMvcApplication.class, args);
+	}
+
+}
